88416c7a65084154cc0c1c2b84c914e4c1b81ff2
Brandon Shiluk <bshiluk@gmail.com>
2019-02-13 20:30:36 -0800

    Add required Wordpress theme files
    Excluding comments.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' );?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php wp_head();?>
+    </head>
+    <body>
+        <div id="app"></div>
+        <?php wp_footer();?>
+    </body>
+</html>
