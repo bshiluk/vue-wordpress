d49e9164df9ae0c8bfb5f6c07b1a758d9840ca67
Brandon Shiluk <bshiluk@gmail.com>
2019-02-18 11:59:14 -0800

    Add MainMenu component

---
+++ src/components/template-parts/MainMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="main-menu">
+    <a
+      v-for="item in menu"
+      :key="item.id"
+      :href="item.url"
+      :target="item.target"
+      :title="item.title"
+      v-html="item.content"
+    ></a>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  computed: {
+    menu() {
+      return this.$store.getters.menu({ name: 'main' })
+    }
+  }
+}
+</script>
