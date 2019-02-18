bcfa9ab653b6610e5fafd209cbafaa043643b64a
Brandon Shiluk <bshiluk@gmail.com>
2019-02-18 12:05:33 -0800

    Update with click handler delegating all internal links to vue-router
    Also, added MainMenu and router-view components.

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,86 @@
 <template>
-  <div id="vue-wordpress-app">
-    <h1>{{ name }}</h1>
-    <p>{{ description }}</p>
+  <div 
+    id="vue-wordpress-app"
+    @click="handleClicks"
+  >
+    <h1>{{ site.name }}</h1>
+    <p>{{ site.description }}</p>
+    <main-menu />
+    <transition
+      name="fade"
+      mode="out-in"
+      @after-leave="updateScroll"
+    >
+      <router-view />
+    </transition>
   </div>
 </template>
 
 <script>
+import MainMenu from '@/components/template-parts/MainMenu'
+
 export default {
+  components: {
+    MainMenu
+  },
   data() {
     return {
-      name: this.$store.state.site.name,
-      description: this.$store.state.site.description
+      site: this.$store.state.site
     }
   },
-  mounted() {
-    console.log('App Mounted')
+  methods: {
+    getLinkEl(el) {
+      while (el.parentNode) {
+        if (el.tagName === 'A') return el
+        el = el.parentNode
+      }
+    },
+    handleClicks (e) {
+      const a = this.getLinkEl(e.target)
+      if (a && a.href.includes(this.site.url)) {
+        const { altKey, ctrlKey, metaKey, shiftKey, button, defaultPrevented } = e
+        // don't handle if has class 'no-router'
+        if (a.className.includes('no-router')) return
+        // don't handle with control keys
+        if (metaKey || altKey || ctrlKey || shiftKey) return
+        // don't handle when preventDefault called
+        if (defaultPrevented) return
+        // don't handle right clicks
+        if (button !== undefined && button !== 0) return
+        // don't handle if `target="_blank"`
+        if (a.target && a.target.includes('_blank')) return
+        // don't handle same page links
+        let currentURL = new URL(a.href, window.location.href)
+        if (currentURL && currentURL.pathname === window.location.pathname) {
+          // if same page link has same hash prevent default reload
+          if (currentURL.hash === window.location.hash) e.preventDefault()
+          return
+        }
+        // Prevent default and push to vue-router
+        e.preventDefault()
+        let path = a.href.replace(this.site.url, '')
+        this.$router.push(path)
+      }
+    },
+    updateScroll() {
+      window.scroll(0,0)
+    }
   }
 }
 </script>
 
 <style>
 
-html {
-font-size: 62.5%;
+/* Vue transition classes
+-------------------------------------------- */
+
+.fade-enter-active {
+  transition: opacity .5s cubic-bezier(.4,0,0,1);
 }
 
-body {
-  margin: 0;
-  font-size: 1.8em;
-  line-height: 1.6;
-  font-weight: 400;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu,Cantarell, "Helvetica Neue", sans-serif;
-  color: #444; 
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.fade-enter,
+.fade-leave-to {
+  opacity: 0
 }
 
 </style>
