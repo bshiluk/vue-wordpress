64fc7861c013e7a8238d2944ac91d297b43533f9
Brandon Shiluk <bshiluk@gmail.com>
2019-02-18 23:27:41 -0800

    Prevent browser from automatically handling scroll position restoration

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import routes from './routes'
 
 const { url } = __VUE_WORDPRESS__.routing
 
+// scroll position is handled in @after-leave transition hook
+if ('scrollRestoration' in window.history) window.history.scrollRestoration = 'manual'
+
 Vue.use(Router)
 
 export default new Router({
