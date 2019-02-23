870ea5aeea43c3ea9d6975e5921aed3a003a3c25
Brandon Shiluk <bshiluk@gmail.com>
2019-02-22 18:21:23 -0800

    Refactor index.php to match rendered header in app
    Additional WP template creation needed for actual server-side rendering.

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
 <html <?php language_attributes();?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' );?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?php wp_head();?>
-    </head>
-    <body>
-        <div id="vue-wordpress-app">
-            <h1><?php echo RADL::get('state.site')['name'];?></h1>
-            <p><?php echo RADL::get('state.site')['description'];?></p>
-        </div>
-        <?php wp_footer();?>
-    </body>
+
+<head>
+  <meta charset="<?php bloginfo( 'charset' );?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head();?>
+</head>
+
+<body>
+  <div id="vue-wordpress-app" class="container">
+    <div class="site-branding">
+      <?php 
+        $logo_id = RADL::get('state.site')['logo'];
+        if ( $logo_id ):
+            $logo = RADL::get('state.media', $logo_id);
+        ?>
+      <img class="logo" src="<?php echo $logo['source_url']; ?>" alt="<?php echo $logo['alt_text']; ?>" />
+      <?php endif;?>
+      <span><?php echo RADL::get('state.site')['name']; ?></span>
+    </div>
+    <nav class="main-menu">
+      <?php echo '<script>console.log('.json_encode(RADL::get('state.menus')).')</script>'; ?>
+      <?php foreach ( RADL::get('state.menus')['main'] as $item ): ?>
+      <a href="<?php echo $item['link']; ?>" target="<?php echo $item['target']; ?>"
+        title="<?php echo $item['title']; ?>"><?php echo $item['content']; ?></a>
+      <?php endforeach;?>
+    </nav>
+    <main>
+    </main>
+  </div>
+  <?php wp_footer();?>
+</body>
+
 </html>
