789af356a8abad6b8b01b3fa3f5efb5dfc5ec412
Brandon Shiluk <bshiluk@gmail.com>
2019-02-22 18:29:28 -0800

    Integrate batching of requests for a single resource of type by id
    Limits excessive and duplicate requests.

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+
 const { url } = __VUE_WORDPRESS__.routing
 
 const ajax = axios.create(
@@ -12,12 +13,38 @@ const ajax = axios.create(
   }
 )
 
-export const fetchSingle = ({ type, params = {} }) => {
-  return ajax.get(`/${type}/`, { params })
+const batchRequest = {}
+
+function addBatchId(type, id) {
+  if ( ! batchRequest[type] ) {
+    batchRequest[type] = {}
+    batchRequest[type].ids = [id]
+    batchRequest[type].request = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(batchRequestIds(type))
+        batchRequest[type] = null
+      }, 100)
+    })
+  } else if( ! batchRequest[type].ids.includes(id) ){
+    batchRequest[type].ids.push(id)
+  }
 }
 
-export const fetchSingleById = ({ type, id }) => {
-  return ajax.get(`/${type}/${id}`)
+function batchRequestIds(type) {
+  return ajax.get(`/${type}/`, { params: { include: batchRequest[type].ids, per_page: 100 } })
+}
+
+export const fetchSingleById = ({ type, id, batch = false }) => {
+  if (batch) {
+    addBatchId(type, id)
+    return batchRequest[type].request
+  } else {
+    return ajax.get(`/${type}/${id}`)
+  }
+}
+
+export const fetchSingle = ({ type, params = {} }) => {
+  return ajax.get(`/${type}/`, { params })
 }
 
 export const fetchItems = ({ type, params = {} }) => {
